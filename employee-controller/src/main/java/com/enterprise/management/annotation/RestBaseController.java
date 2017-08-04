f1d074f53fae314249596a328c6ec599014a5a96
Javi Mora <javier.mora@linkeit.com>
2017-08-04 09:59:58 +0200

    create custom annotation

--- employee-controller/src/main/java/com/enterprise/management/annotation/RestBaseController.java
+++ employee-controller/src/main/java/com/enterprise/management/annotation/RestBaseController.java
@@ -1,7 +1,22 @@
 package com.enterprise.management.annotation;
 
-/**
- * Created by javimora on 4/8/17.
- */
-public interface RestBaseController {
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Api
+@Controller
+@RequestMapping
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface RestBaseController {
+
+    String value() default "/";
+    String description() default "Operations controllers";
 }
